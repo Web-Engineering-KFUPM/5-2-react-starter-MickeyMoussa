e86c1c9487cfbf0001c21b5da831576b38d09e8d
MickeyMoussa <s202349310@kfupm.edu.sa>
2025-10-01 09:26:59 +0300

    done

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,13 @@
+function StudentCard({name, id, dept}){
+
+    return(
+        <div>
+        <h3>{name}</h3>
+        <p>{id}</p>
+        <p>{name}</p>
+        </div>
+    );
+
+}
+
+export default StudentCard;
